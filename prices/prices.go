e6bc54cc4fde96da140dce93047a698b941b1b42
jvsena42 <jvsena16@gmail.com>
2024-03-24 11:17:44 -0300

    create Process meethod

--- prices/prices.go
+++ prices/prices.go
@@ -1,9 +1,23 @@
 package prices
 
+import (
+	"fmt"
+)
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) Process() map[string]float64 {
+	result := make(map[string]float64, len(job.InputPrices))
+
+	for _, price := range job.InputPrices {
+		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+	}
+
+	return result
 }
 
 func NewTaxInclidedPriceJob(taxRate float64) *TaxIncludedPriceJob {
