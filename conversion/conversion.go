e247112441514e6f750f17ea244be50eba7e8ff8
jvsena42 <jvsena16@gmail.com>
2024-03-24 15:42:58 -0300

    feat: implement write file logic

--- conversion/conversion.go
+++ conversion/conversion.go
@@ -4,7 +4,7 @@ import (
 	"strconv"
 )
 
-func stringToFloat(strings []string) ([]float64, error) {
+func StringToFloat(strings []string) ([]float64, error) {
 	floatList := make([]float64, len(strings))
 
 	for stringIndex, stringVal := range strings {
--- prices/prices.go
+++ prices/prices.go
@@ -10,7 +10,7 @@ import (
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
-	TaxIncludedPrices map[string]float64
+	TaxIncludedPrices map[string]string
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
@@ -42,7 +42,8 @@ func (job *TaxIncludedPriceJob) Process() {
 		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.3f", taxIncludedPrice)
 	}
 
-	fmt.Println(result)
+	job.TaxIncludedPrices = result
+	filemanager.WriteJson(fmt.Sprintf("result_%.0f.json", job.TaxRate*100), job)
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
